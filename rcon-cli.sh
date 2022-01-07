6e3029559b90e052c9197bf6ae0070d8518f12f9
Jason Kridner <jkridner@beagleboard.org>
2022-01-07 13:39:04 -0500

    I don't know what matters

--- rcon-cli.sh
+++ rcon-cli.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-docker exec -i bc rcon-cli
+version https://git-lfs.github.com/spec/v1
+oid sha256:86cb52c47fa37fab55288e08353484e371e876401cf27e6254f1d8fca14cf2cb
+size 38
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-docker start bc
+version https://git-lfs.github.com/spec/v1
+oid sha256:5118cedb615250c1d46563abe42ef5c0f8d815937d1b81e34cdfa899a8c1966a
+size 29
