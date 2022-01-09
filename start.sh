2ba33f47147f149ebe105636ad7c3b105a7783ff
Jason Kridner <jkridner@beagleboard.org>
2022-01-08 21:34:13 -0500

    Git-LFS is a mess

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3667d91ba91b73991036f5772660828f6c55146bc9485a44d959dd046544795
-size 35
+oid sha256:3e70ce493bbcd9fcdd19cd0bb8982ab250bf3bac2812588c9509f5fdb9f77ca5
+size 33
