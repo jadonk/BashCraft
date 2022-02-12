10696f6d9d40616c87bc5b297bcc9c0f3034b9b8
Jason Kridner <jkridner@beagleboard.org>
2022-02-11 22:04:39 -0500

    on no!

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6a3cec28864ce52a636be3085c95ba12909366c833ebf64b041bade2a90bec94
-size 258
+#!/bin/sh
+docker run -d -v /home/jkridner/workspace/bashcraft:/data \
+    --dns 192.168.0.1 \
+    -e TYPE=FORGE \
+    -e MEMORY=4G \
+    -e VERSION=1.16.5 -e FORGEVERSION=36.1.0 \
+    -p 25565:25565 -e EULA=TRUE --name bashcraft itzg/minecraft-server:java8
+
--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3e70ce493bbcd9fcdd19cd0bb8982ab250bf3bac2812588c9509f5fdb9f77ca5
-size 33
+#!/bin/sh
+docker start bashcraft
