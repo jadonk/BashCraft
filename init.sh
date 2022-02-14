4222ca9089211833a3e9880adfbf3b2061870068
Jason Kridner <jkridner@beagleboard.org>
2022-02-14 14:11:47 -0500

    Save

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ docker run -d -v /home/jkridner/workspace/bashcraft:/data \
     --dns 192.168.0.1 \
     -e TYPE=FORGE \
     -e MEMORY=4G \
-    -e VERSION=1.16.5 -e FORGEVERSION=36.1.0 \
+    -e VERSION=1.16.5 -e FORGEVERSION=36.2.26 \
     -p 25565:25565 -e EULA=TRUE --name bashcraft itzg/minecraft-server:java8
 
