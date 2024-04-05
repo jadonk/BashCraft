677c50800c9df33f07e8534d40c937a29642835c
Jason Kridner <jkridner@beagleboard.org>
2024-04-05 16:02:23 -0400

    snap before resort

---
+++ unknown-world/computercraft/computer/2/dotorch.lua
@@ -0,0 +1,12 @@
+for i = 1, 64 do
+ turtle.turnLeft()
+ turtle.forward()
+ turtle.forward()
+ turtle.forward()
+ turtle.forward()
+ turtle.forward()
+ turtle.forward()
+ turtle.turnRight()
+ turtle.select(1)
+ turtle.place()
+end
